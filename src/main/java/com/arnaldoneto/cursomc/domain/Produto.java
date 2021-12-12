1b5b0e5ffe35593a2680e4444b506107c8cdaa49
Arnaldo Neto <vilela-neto@hotmail.com>
2021-12-12 15:55:19 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/arnaldoneto/cursomc/domain/Produto.java
+++ src/main/java/com/arnaldoneto/cursomc/domain/Produto.java
@@ -15,6 +15,7 @@ import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Produto {
@@ -35,6 +36,7 @@ public class Produto {
 	
 	private List<Categoria> categorias = new ArrayList<>();
 	
+	@JsonIgnore
 	@OneToMany(mappedBy="id.produto")
 	private Set<ItemPedido> itens = new HashSet<>();
 	
@@ -49,6 +51,7 @@ public class Produto {
 		this.preco = preco;
 	}
 	
+	@JsonIgnore
 	public List<Pedido> getPedidos(){
 		List<Pedido> lista = new ArrayList<>();
 		for(ItemPedido x : itens) {
